a08245de72a0db1c6d3140919972ff0c2b9f3763
Mustafa <mustafa@backbase.com>
2019-10-03 22:31:38 +0200

    Initial Setup

---
+++ Config/ConfigExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConfigExamples
+//
+//  Created by Mustafa Karakus on 03/10/2019.
+//  Copyright © 2019 Mustafa Karakus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
