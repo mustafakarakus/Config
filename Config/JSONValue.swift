40d4d7e9e1d908d53bf20f610db15c1706a62158
Mustafa Karakus <mustafa@backbase.com>
2021-11-27 14:25:54 +0100

    PropertyWrappers was using Singleton Config, but now we don't have public singleton Config anymore. Singleton moved under internal usage for PropertyWrappers, since we can't pass an instance to PropertyWrapper

--- Config/JSONValue.swift
+++ Config/JSONValue.swift
@@ -12,15 +12,38 @@ import Foundation
 public struct JSONValue<Value> {
     private let key: String
     private var value: Value
+    
     public var wrappedValue: Value {
-        get { Config.shared[key] ?? value }
+        get { Config.shared?[key] ?? value }
         set { value = newValue }
     }
 }
 
 extension JSONValue {
-    public init(wrappedValue:Value, _ key:String) {
+    public init(wrappedValue: Value, _ configFileName: String, _ key: String) {
         self.value = wrappedValue
         self.key = key
+        Config.initialize(name: configFileName)
+    }
+    
+    public init(wrappedValue: Value, url: String, _ key: String) {
+        self.value = wrappedValue
+        self.key = key
+        guard let url = URL(string: url) else { return }
+        Config.initialize(url: url)
+    }
+}
+
+extension Config {
+    internal static var shared: Config? = nil
+    internal static func initialize(name: String) {
+        if shared == nil {
+            shared = Config(with: name)
+        }
+    }
+    internal static func initialize(url: URL) {
+        if shared == nil {
+            shared = Config(with: url)
+        }
     }
 }
